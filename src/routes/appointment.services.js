40e12480fc34b5d1b9774d2fa5465eed5b9d5724
Maria Bonife Ng <mng@yondu.com>
2024-10-23 14:10:27 +0800

    adding initial dev in appointment settler

--- src/routes/appointment.services.js
+++ src/routes/appointment.services.js
@@ -53,4 +53,71 @@ router.post("/save", async (req, res) => {
     });
   }
 });
+
+
+
+
+router.post("/available-doctor", async (req, res) => {
+  const payload = req.body;
+
+  try {
+    //get doctor id
+    //get available dates and time based on duration
+
+    res.status(201).send({
+      status: true,
+      message: `Ongoing Development`,
+    });
+
+
+  } catch (error) {
+    return res.status(500).send({
+      status: false,
+      message: "Failed to fetch data from external API",
+    });
+  }
+});
+
+router.post("/appointment-settler", async (req, res) => {
+  const payload = req.body;
+
+  try {
+    //get doctor id
+    //get patient id
+    //set status active
+    //date
+    //time
+    //priorityop
+
+    res.status(201).send({
+      status: true,
+      message: `Ongoing Development`,
+    });
+
+  } catch (error) {
+    return res.status(500).send({
+      status: false,
+      message: "Failed to fetch data from external API",
+    });
+  }
+});
+
+router.post("/cancel", async (req, res) => {
+  const payload = req.body;
+
+  try {
+    //MAKE THE APPOINTMENT in CANCEL STATUS
+    //INITIATE ANOTHER APOINTMENTS BASED ON PRIORITY.
+    res.status(201).send({
+      status: true,
+      message: `Ongoing Development`,
+    });
+  } catch (error) {
+    return res.status(500).send({
+      status: false,
+      message: "Failed to fetch data from external API",
+    });
+  }
+});
+
 export default router;
