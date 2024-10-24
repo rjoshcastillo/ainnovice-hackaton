16e0c3f66e7d1cbbdbaee10100bb163b15759e88
Maria Bonife Ng <mng@yondu.com>
2024-10-24 14:06:09 +0800

    development appointment settler

--- src/routes/appointment.services.js
+++ src/routes/appointment.services.js
@@ -123,23 +123,65 @@ router.post("/available-doctor", async (req, res) => {
     });
   }
 });
-
+// PARAMETERS
+// {
+//   "account_id": "7",
+//   "doctor_id": "7",
+//   "alcohol_consumption": "heavy",
+//   "smoking": "",
+//   "height": "",
+//   "weight": "",
+//   "breathing_trouble": "",
+//   "pain_level": "",
+//   "pain_part": "",
+//   "medical_concern": "",
+//   "symptoms": "",
+//   "temperature": "",
+//   "appointment_date": "2024-10-25",
+//   "appointment_start": "08:00",
+//   "appointment_end": "08:30",
+//   "urgency": "5"
+// }
 router.post("/appointment-settler", async (req, res) => {
   const payload = req.body;
 
-  try {
-    //get doctor id
-    //get patient id
-    //set status active
-    //date
-    //time
-    //priority
+  try{
+      const query = `
+      INSERT INTO appointments 
+      (account_id, doctor_id, alcohol_consumption, smoking, height, weight,
+       breathing_trouble, pain_level, pain_part, medical_concern, symptoms, temperature,appointment_date, appointment_start,appointment_end,urgency)  
+      VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? ,? ,?)
+    `;
+    db.query(query,[
+      payload.account_id,
+      payload.doctor_id,
+      payload.alcohol_consumption,
+      payload.smoking,
+      payload.height,
+      payload.weight,
+      payload.breathing_trouble,
+      payload.pain_level,
+      payload.pain_part,
+      payload.medical_concern,
+      payload.symptoms,
+      payload.temperature,
+      payload.appointment_date,
+      payload.appointment_start,
+      payload.appointment_end,
+      payload.urgency,
+    ],(error, results) => {
+      if (error)
+        return res.status(500).send({
+          status: false,
+          message: `Failed saving appointment ${error}`,
+        });
+      res.status(201).send({
+        status: true,
+        message: `Appointment saved with priority score of ${payload.urgency}`,
+        data: results
+      });
 
-    res.status(201).send({
-      status: true,
-      message: `Ongoing Development`,
     });
-
   } catch (error) {
     return res.status(500).send({
       status: false,
